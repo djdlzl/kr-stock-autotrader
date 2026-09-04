4e2fa1e0a11e6706d4addc8f5c6214b4974334e8
Jaewoo Cho <djdlzl93@gmail.com>
2026-09-04 11:55:13 +0900

    revert: remove wrong-topic giraffe prototype

--- kr_stock_autotrader/ui.py
+++ kr_stock_autotrader/ui.py
@@ -16,4 +16,3 @@ const $=s=>document.querySelector(s),alertEl=$('#alert'),form=$('#auth-form'),pa
 </script></body></html>'''
 
 APP_HTML = Path(__file__).with_name('decision_card_app.html').read_text(encoding='utf-8')
-PROTOTYPE_HTML = Path(__file__).with_name('prototype.html').read_text(encoding='utf-8')
